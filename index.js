f5c5bff1cae8b7ffa0ac25864e39ca6bd03a6ff7
asif93-138 <asif93@student.sust.edu>
2024-02-14 21:43:05 +0600

    get request testing for json file..

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
+const test = require('./testData.json');
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('<h1>Server is running!</h1>')
 })
+
+app.get('/test', (req, res) => {
+    res.send(test)
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
